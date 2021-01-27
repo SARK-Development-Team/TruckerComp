4406b2cb5fa2764594fe99f534011008b775f5ea
Brhymer <leiqien28@hotmail.com>
2021-01-26 17:15:32 -0800

    test commit to deploy to heroku

--- form-submission.js
+++ form-submission.js
@@ -4,4 +4,36 @@ defaultDate();
 
 function defaultDate() {
     document.getElementById("startDate").valueAsDate = new Date();
-  }
+}
+
+// The following code saves the form data in document.cookie
+
+const today = new Date();
+const expiry = new Date(today.getTime() + 24 * 3600000); // saves cookie for 24 hours
+
+function setCookie(name, value) {
+  document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+}
+
+
+//serialize formdata
+
+// formData.JSON
+// for (var pair of formData.entries()) {
+//   console.log(pair[0] + ': ' + pair[1]);
+// }
+
+async function saveFormData (e) {
+  e.preventDefault();
+  const formData = new FormData(document.querySelector('form'));
+
+  const object = {};
+  formData.forEach((value, key) => object[key] = value);
+  var json = JSON.stringify(object);
+
+  // console.log(formData);
+  
+  const cache = await caches.open('joid');
+  cache.add('/formsubmission.json');
+  // setCookie("clientData", formData);
+}
