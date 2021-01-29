5f2c4ddee2c56c2bff5282fa05d14db04fdb0131
Brhymer <leiqien28@hotmail.com>
2021-01-29 13:47:39 -0800

    adds info blurb toggle functionality

--- about-section.js
+++ about-section.js
@@ -2,15 +2,33 @@
 
 const body=document.getElementsByTagName("body")[0];
 const about=document.getElementById("about");
-const bottom=document.getElementById("bottom");
+const info=document.getElementById("info");
 
 body.onscroll = function(){
     if (document.documentElement.scrollTop>=550) {
         about.style.opacity=1;
     }
-    if (document.documentElement.scrollTop>=900) {
-        bottom.style.opacity=1;
+    if (document.documentElement.scrollTop>=1300) {
+        info.style.opacity=1;
     }
 
 };
 
+// This toggles the display of the two-item info blurb
+function toggleBlurb() {
+    const blurb1 = document.getElementById('blurb1');
+    const blurb2 = document.getElementById('blurb2');
+    if (blurb1.style.display=='block') {
+        console.log('trigger1');
+        blurb1.style.display='none';
+        blurb1.style.opacity=0;
+        blurb2.style.display='block';
+        blurb2.style.opacity=1;
+    } else {
+        console.log('trigger2');
+        blurb1.style.display='block';
+        blurb1.style.opacity=1;
+        blurb2.style.display='none';
+        blurb2.style.opacity=0;
+    }
+}
