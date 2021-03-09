4e6e696749fc649f98e87a81d8cd72a1aed1eb19
Brhymer <leiqien28@hotmail.com>
2021-03-09 13:21:56 -0800

    update for heroku troubleshooting cont.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const flash = require('connect-flash');
 
 // for environment variables
 require('dotenv').config()
