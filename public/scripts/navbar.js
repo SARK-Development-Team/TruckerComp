3cec7f6cb24a35e936d53698242fbc3ddec6df97
Brhymer <leiqien28@hotmail.com>
2021-04-01 12:41:18 -0700

    new color scheme, navbar transition

---
+++ public/scripts/navbar.js
@@ -0,0 +1,18 @@
+const navbar = document.getElementById("navbar");
+const comp = document.getElementById("comp");
+
+
+window.onscroll = () => { 
+    "use strict";
+    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20 ) {
+        comp.style.color=("#F5F3F5");
+        navbar.classList.add("darkNav");
+        navbar.classList.remove("lightNav");
+        console.log("scrolled")
+    } 
+    else {
+        comp.style.color=("rgb(33, 115, 209)");
+        navbar.classList.add("lightNav");
+        navbar.classList.remove("darkNav");
+    }
+};
