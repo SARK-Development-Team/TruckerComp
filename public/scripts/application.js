1da06235f7b2d99debbd35cec428f6d204630461
Brhymer <leiqien28@hotmail.com>
2021-06-21 13:03:18 -0700

    update to dashboard sections, application section, trial introduction of chat app

---
+++ public/scripts/application.js
@@ -0,0 +1,51 @@
+
+
+function toggleSection(sectionID) {
+    const appSections = Array.from(document.getElementsByClassName('application-section'));
+
+    appSections.forEach(element => {
+        element.classList.remove('visible');    
+    });
+    document.getElementById(sectionID).classList.add('visible');
+
+}
+
+// Search the mongo DB for the current user data
+async function mongoSearch(email) {
+    const data = {'email': email};
+    const result = await fetch('mongoSearch', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    return result;
+}
+
+const userEmail = document.getElementById('dbScript').getAttribute('data-email');
+
+async function loadUser(email) {
+    // search in Mongo DB
+    const user = await mongoSearch(email);
+
+    return user;
+}
+
+function displayVisualProgress(stage) {
+    const progressValue = `${stage * 20}%`;
+    document.getElementById('progress-bar').style.width=progressValue;
+    document.getElementById('progress-value').innerText=progressValue;
+}
+
+window.onload = async ()=> {
+    let user = await loadUser(userEmail);
+    // Determine user stage
+    if (user.stage) {
+        
+        displayVisualProgress(user.stage);
+
+    } else {
+        displayVisualProgress(1);
+    }
+
+
+}
