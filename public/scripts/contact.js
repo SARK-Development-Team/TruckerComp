adc826565aa39f279d77aa80a72080d334ac8a0b
Brhymer <leiqien28@hotmail.com>
2021-07-06 10:03:42 -0700

    adds chat window to contact page

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -21,3 +21,24 @@ function sendContact(e) {
     alert("Thank you! A SARK employee will reach out to you within 1-2 business days");
     window.location.reload();
 }
+
+// document.getElementById('chat-window').addEventListener('mouseenter', (e) => {
+//     console.log(e)
+//     console.log(e.target)
+// })
+
+window.onload = () => {
+    defineChatWindow();
+
+}
+// setTimeout(()=> defineChatWindow(), 3000);
+
+function defineChatWindow() {
+    const chatWindow = document.getElementsByTagName('iframe')[0]
+    chatWindow.addEventListener('mouseenter', (e) => {
+        e.target.style.height='500px';
+    })
+    chatWindow.addEventListener('mouseleave', (e) => {
+        e.target.style.height='220px';
+    })
+}
